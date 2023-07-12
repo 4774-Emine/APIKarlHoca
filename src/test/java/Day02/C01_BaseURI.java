9a2f5abee2b8ee2acdca5ba5b0f251c8d5bafcb5
EmineDisceken <haticehifa.2018@gmail.com>
2023-07-12 17:11:45 +0300

    12.07

--- src/test/java/Day02/C01_BaseURI.java
+++ src/test/java/Day02/C01_BaseURI.java
@@ -1,23 +1,29 @@
 package Day02;
-
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
 
 public class C01_BaseURI {
 
-    //GET https://petstore.swagger.io/v2/store/inventory
+
+        // GET /store/inventory
+
+        // URL -> base URI + endpoint + query
 
     @Test
-    public void baseURIUsage() {
-//Base URI reguest gönderilirken url in önüne eklenecek olan base URI bilgisini requeste ekler
+    public void baseURIUsage(){
+
+        // Base URI request gonderilirken url in onune eklenecek olan base URI bilgisini requeste ekler
+
         given().
                 accept(ContentType.JSON).
                 baseUri("https://petstore.swagger.io/v2").
-                when().get("/store/inventory").then().log().all();
+                when().
+                get("/store/inventory").
+                then().
+                log().
+                all();
 
     }
-
 
 }
